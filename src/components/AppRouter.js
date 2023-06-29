1961f472b426e8b8c6a37ef53b90feb4c07acdc1
AnJW-HGU <22000404@handong.ac.kr>
2023-06-29 16:53:38 +0900

    feat: Create sign out

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -2,13 +2,29 @@ import React, { useState } from "react";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "routes/Home";
 import Auth from "routes/Auth";
+import Navigation from "./Navigation";
+import Profile from "routes/Profile";
 
 const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Routes>
         {isLoggedIn ? (
-          <Route exact path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
+          <>
+            <Route
+              exact
+              path={`${process.env.PUBLIC_URL}/`}
+              element={<Home />}
+            />
+            <Route
+              exact
+              path={`${process.env.PUBLIC_URL}/profile`}
+              rreplace
+              to={`${process.env.PUBLIC_URL}/`}
+              element={<Profile />}
+            />
+          </>
         ) : (
           <Route exact path={`${process.env.PUBLIC_URL}/`} element={<Auth />} />
         )}
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,7 +1,20 @@
+import { authService } from "fbase";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Profile = () => {
-  return <span>Profile</span>;
+  const navigate = useNavigate();
+
+  const onLogOutClick = () => {
+    authService.signOut();
+    navigate(-1);
+    // navigate(`${process.env.PUBLIC_URL}/`);
+  };
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
 };
 
 export default Profile;
